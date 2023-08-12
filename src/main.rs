2e7f5e0c8a373ff124f07f03c8da466ad50b4353
Akihiro <isoiso0316aaa@gmail.com>
2023-08-13 01:23:22 +0900

    a

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
-
+ 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
